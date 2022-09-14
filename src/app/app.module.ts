e8899ef3b18c4b6397c564dd490fae3765e96c63
pratibhashelke001 <pratibhashelke001@gmail.com>
2022-09-14 00:16:32 -0700

    angular-new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProjectNameComponent } from './components/project-name/project-name.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectNameComponent
   ],
   imports: [
     BrowserModule,
